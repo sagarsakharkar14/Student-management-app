0f067917b66646a2f46564a0173808bcfffdef3e
sagarsakharkar14 <137693749+sagarsakharkar14@users.noreply.github.com>
2025-08-30 09:41:51 +0530

    Add files via upload

---
+++ djangocrud/crudapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Student-Name')
+    email = models.EmailField(max_length=254, verbose_name='Student-Email')
+
+    def __str__(self):
+        return str(self.id)
+    
